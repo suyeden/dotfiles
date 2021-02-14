2cb0cd2fb614eb5e7cd4ca2f88feb59e1fc10728
suyeden <nagasuesue@yahoo.co.jp>
2021-02-14 23:54:32 +0900

    Latest version

--- Emacs/dot.emacs.d/win-custom.el
+++ Emacs/dot.emacs.d/win-custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (web-mode js2-mode auto-complete undo-tree dirtree ir-black-theme))))
+   '(grip-mode magit web-mode js2-mode auto-complete undo-tree dirtree ir-black-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
