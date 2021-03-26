0d242db5cdb9fc8bce5e33453814c4761b03abeb
suyeden <nagasuesue@yahoo.co.jp>
2021-03-26 12:01:29 +0900

    Latest update

---
+++ Emacs/dot.emacs.d/elpa/grip-mode-2.2.1/grip-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from z:/.emacs.d/elpa/grip-mode-2.2.1/grip-mode.el  -*- no-byte-compile: t -*-
+(define-package "grip-mode" "2.2.1" "Instant GitHub-flavored Markdown/Org preview using grip." '((emacs "24.4")) :commit "9615c4774727a719d38313a679d70f2a2c6aca68" :authors '(("Vincent Zhang" . "seagle0128@gmail.com")) :maintainer '("Vincent Zhang" . "seagle0128@gmail.com") :keywords '("convenience" "markdown" "preview") :url "https://github.com/seagle0128/grip-mode")
